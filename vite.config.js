4c6ee12b681ddd9e211ef3bb4fae5bcf7ebda3f4
Jay <kkvia@msn.com>
2026-01-02 14:33:21 +0800

    chore: rename project to Jay-Vision-Pro and update base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // 必須與你的 GitHub Repo 名稱一致
-  base: '/comb-engine-simulator/', 
+  base: '/Jay-Simulation-Suite/', 
   plugins: [react()],
 })
